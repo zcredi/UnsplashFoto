8f2070149cc3278269af0fad35abcaba416ee9bd
Vladislav <107634092+zcredi@users.noreply.github.com>
2024-02-08 19:32:03 +0300

    Initial Commit

---
+++ UnsplashFoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnsplashFoto
+//
+//  Created by Владислав on 08.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
